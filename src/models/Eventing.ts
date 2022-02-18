be466648bcbc876210de34cb15026df0da0d58ce
SahilMahadwar <accounts@mahadwar.com>
2022-02-19 00:09:19 +0530

    Composition Refactor

---
+++ src/models/Eventing.ts
@@ -0,0 +1,24 @@
+type CallBack = () => void;
+
+export class Eventing {
+  events: { [key: string]: CallBack[] } = {};
+
+  on(eventName: string, callback: CallBack): void {
+    //   this.events[eventName] // callback[ ] or undefined
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
+}
