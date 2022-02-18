9c8f3deef93c9d9e36bbb9936795e0646c71fe67
SahilMahadwar <accounts@mahadwar.com>
2022-02-19 00:08:56 +0530

    Composition Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'RICK' });
+const user = new User({ name: 'RICK', age: 0 });
 
-user.save();
+user.attributes.get('id');
+
+user.sync.save({});
